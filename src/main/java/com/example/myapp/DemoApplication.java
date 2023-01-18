c773e8abde8ebda1cbd452e134007e3e59d75971
garchomp-game <otoka.japanesedrum@gmail.com>
2023-01-18 17:49:03 +0900

    commit

--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -3,11 +3,21 @@ package com.example.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(
+  title = "Pokedex API",
+  version = "1.0.1",
+  description = "ポケジニア、ルギアのめぎはさん(twitter: @kyoswin7)の" +
+  "jsonをAPI化しました"
+  )
+)
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
 }
